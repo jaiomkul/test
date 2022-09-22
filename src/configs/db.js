2caafec9a4ef75d6c7895c29d34e337f32087102
jaiomkul <94021305+jaiomkul@users.noreply.github.com>
2022-09-22 15:18:51 +0530

    make it better

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect(process.env.MDB);
+  return mongoose.connect(
+    "mongodb+srv://jpg:jpg123@ok.dxdgipu.mongodb.net/mini?retryWrites=true&w=majority"
+  );
 };
 
 module.exports = connect;
