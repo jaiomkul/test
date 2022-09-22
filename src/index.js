e098609886e6f521428870da850492b5e5bcccd9
jaiomkul <94021305+jaiomkul@users.noreply.github.com>
2022-09-22 19:45:20 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const productsController = require("./controllers/product.controller");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/products", productsController);
 
